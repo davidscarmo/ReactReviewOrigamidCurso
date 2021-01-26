73b5e536421d6795c7fe1acba1ad56934c383cd6
David Carmo <davidsc2812@gmail.com>
2021-01-26 08:02:21 -0300

    Conceitos React
    Foram vistos os conceitos iniciais do react, além de configuração de webpack e do babel.

---
+++ webpack_babel/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+import './App.css'; 
+const App = () => 
+{
+    // return React.createElement('a', {href: 'https://www.google.com.br'}, 'Google') ; 
+    return <a href="https://google.com.br">Google</a>
+}; 
+
+export default App; 
