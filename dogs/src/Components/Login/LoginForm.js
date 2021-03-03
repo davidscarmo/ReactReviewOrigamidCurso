75b6b5690248bef74a6968710f0b9ffb692d0c70
David Carmo <davidsc2812@gmail.com>
2021-03-02 21:57:14 -0300

    user Context
    Adição do context do usuário.

--- dogs/src/Components/Login/LoginForm.js
+++ dogs/src/Components/Login/LoginForm.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { TOKEN_POST, USER_GET } from "../../api";
 import useForm from "../../Hooks/useForm";
+import { UserContext } from "../../UserContext";
 import Button from "../Forms/Button";
 import Input from "../Forms/Input";
 
@@ -9,35 +10,12 @@ const LoginForm = () => {
   const username = useForm();
   const password = useForm();
   
-  const getUser = async(token) =>
-  {
-    const {url, options} = USER_GET(token);
-    const response = await fetch(url, options); 
-    const json = await response.json();
-    console.log(json);
-  }
-  React.useEffect(() => 
-  {
-    const token = window.localStorage.getItem('token');
-    if(token)
-    {
-      getUser(token);
-    }
-  },[]);
+  const {userLogin} = React.useContext(UserContext);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      });
-
-      const response = await fetch(url, options); 
-      const json = await response.json();
-
-      window.localStorage.setItem('token', json.token);
-      getUser(json.token);
+        userLogin(username.value, password.value);
     }
   };
 
